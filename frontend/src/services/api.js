9be7b034ce6dde3b0cd856c4e3cc750bf2b08760
tharushi lakshani <tharushilakshani81@gmail.com>
2025-09-06 13:52:27 +0530

    Chang http API_URL to https URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 //front-end/src/services/api.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:3001/api';
 
 // Create axios instance
 const api = axios.create({
